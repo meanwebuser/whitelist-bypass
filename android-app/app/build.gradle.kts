8a32f32856cc84a7aa4d034eb1b0efabede3364a
roomhacker <roomhacker@localhost>
2026-06-01 01:05:48 +0300

    Default split tunnel and move SOCKS copy to settings

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -36,7 +36,7 @@ val appBrandName = brandEnvValue(brandDotEnv, "APP_BRAND", "BRANDING", "BRAND_NA
 
 val versionMajor = 0
 val versionMinor = 3
-val versionPatch = 8
+val versionPatch = 9
 val versionBuild = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 0
 
 android {
